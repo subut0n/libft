a0c7141eedf07e2f72bf13d7b25d19e01c75ddea
subut0n <a.dzikowski@outlook.fr>
2020-12-02 11:04:13 +0100

    modif 02/12 11:00

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,32 @@
 /*   By: addzikow <addzikow@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 16:06:05 by addzikow          #+#    #+#             */
-/*   Updated: 2020/11/26 16:06:06 by addzikow         ###   ########lyon.fr   */
+/*   Updated: 2020/12/02 11:01:35 by addzikow         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+
+	if (dst == NULL || src == NULL)
+		return (NULL);
+	i = 0;
+	while (src[i])
+		i++;
+	if (dstsize == 0)
+		return (i);
+	i = 0;
+	while (src[i] && i < dstsize - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	i = 0;
+	while (src[i])
+		i++;
+	return (i);
+}
