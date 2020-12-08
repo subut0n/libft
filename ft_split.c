a3868e91d9a3bc27846b1e20ddd82a62e336aaeb
subut0n <a.dzikowski@outlook.fr>
2020-12-08 18:54:48 +0100

    fix ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,12 @@
 /*   By: addzikow <addzikow@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 16:04:57 by addzikow          #+#    #+#             */
-/*   Updated: 2020/12/08 17:39:17 by addzikow         ###   ########lyon.fr   */
+/*   Updated: 2020/12/08 18:51:26 by addzikow         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 static int find_numb_strings(char const *s, char c)
 {
@@ -23,6 +24,8 @@ static int find_numb_strings(char const *s, char c)
     {
         while (s[i] != c)
         {
+            if (!s[i])
+                return (nbrWords);
             if (s[i + 1] == c || s[i + 1] == '\0')
                 nbrWords++;
             i++;
@@ -32,12 +35,12 @@ static int find_numb_strings(char const *s, char c)
     return (nbrWords);
 }
 
-static void free_malloc(char **tab)
+static void free_malloc(char **tab, int j)
 {
     int i;
 
     i = 0;
-    while (tab)
+    while (i < j - 1)
     {
         free(tab[i]);
         i++;
@@ -55,15 +58,23 @@ static int malloc_strings(char const *s, char c, char **tab)
     wordlen = 0;
     while (s[i])
     {
-        while (s[i] != c && s[i++])
+        // fflush(stdout);
+        // printf("i = %d\n", i);
+        // printf("j = %d\n", j);
+        while (s[i + wordlen] != c && s[i + wordlen])
             wordlen++;
-        if(wordlen > 0 && !(tab[j++] = malloc(sizeof(char) * (wordlen + 1))))
+        if(wordlen > 0)
         {
-            free_malloc(tab);
-            return (0);
+            if (!(tab[j] = malloc(sizeof(char) * (wordlen + 1))))
+            {        
+                free_malloc(tab, j);
+                return (0);
+            }
+            j++;
+            i += wordlen;
         }
         wordlen = 0;
-        if (s[i] == c)
+        while (s[i] == c)
             i++;
     }
     return (1);
@@ -80,12 +91,12 @@ static void copy_strings(char const *s, char c, char **tab)
     k = 0;
     while (s[i])
     {
-        while (s[i] != c)
+        while (s[i] != c && s[i])
             tab[j][k++] = s[i++];
         if (k > 0)
         {
-            j++;
             tab[j][k] = '\0';
+            j++;
         }
         while (s[i] == c)
             i++;
@@ -102,7 +113,7 @@ char    **ft_split(char const *s, char c)
     nbr_strings = find_numb_strings(s, c);
     if(!(tab = malloc(sizeof(char *) * (nbr_strings + 1))))
         return (NULL);
-    tab[nbr_strings] = 0;
+    tab[nbr_strings] = NULL;
     is_malloc_ok = malloc_strings(s, c, tab);
     if (is_malloc_ok == 0)
     {
@@ -113,3 +124,21 @@ char    **ft_split(char const *s, char c)
         copy_strings(s, c, tab);
     return (tab);
 }
+
+
+// int main()
+// {
+//     char *s = "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse";
+//     int n;
+//     printf("%d\n", n = find_numb_strings(s, ' '));
+//     char **t = malloc((n+1) * sizeof(char*));
+//     t[n] = NULL;
+//     malloc_strings(s, ' ', t);
+//     copy_strings(s, ' ', t);
+//     while (*t)
+//     {
+//         printf("%s\n", *t);
+//         t++;
+//     }
+//     return (0);
+// }
