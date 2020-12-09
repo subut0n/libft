ea8b2ec17549fa7fe30d9bbc84b8f11080d96ce8
subut0n <a.dzikowski@outlook.fr>
2020-12-09 17:37:59 +0100

    premier pas finalisation

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: addzikow <addzikow@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/09 17:34:39 by addzikow          #+#    #+#             */
+/*   Updated: 2020/12/09 17:34:45 by addzikow         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void ft_lstiter(t_list *lst, void (*f)(void *))
 {
     if (lst == NULL)
         return ;
-    while(lst->next != NULL)
+    while(lst)
     {
         f(lst->content);
         lst = lst->next;
--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: addzikow <addzikow@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/07 14:30:32 by addzikow          #+#    #+#             */
-/*   Updated: 2020/12/09 14:00:00 by addzikow         ###   ########lyon.fr   */
+/*   Updated: 2020/12/09 17:35:40 by addzikow         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,21 @@
 
 t_list *ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *))
 {
-    t_list *element;
+    t_list *list;
+    t_list *new_list;
 
-    if (!lst || !f)
-        return (NULL);
-    element = lst;
-    while (element)
+    list = lst;
+    new_list = list;
+    while (list)
     {
-        element;
-        
-        
+        if(!(new_list = ft_lstnew(f(list->content))))
+        {
+            ft_lstclear(&list, del);
+            free(list);
+        }
+        ft_lstadd_back(&list, new_list);
+        list = list->next;
+        new_list = new_list->next;
     }
+    return (new_list);
 }
