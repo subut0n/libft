01c74e271f3c4a9dbeeca2b00a7eeeb157e6a9b4
subut0n <a.dzikowski@outlook.fr>
2020-12-18 12:03:50 +0100

    last home add

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,29 +6,34 @@
 /*   By: addzikow <addzikow@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/07 14:30:32 by addzikow          #+#    #+#             */
-/*   Updated: 2020/12/09 17:35:40 by addzikow         ###   ########lyon.fr   */
+/*   Updated: 2020/12/18 12:02:52 by addzikow         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-    t_list *list;
-    t_list *new_list;
+	t_list	*new_lst;
+	t_list	*save;
 
-    list = lst;
-    new_list = list;
-    while (list)
-    {
-        if(!(new_list = ft_lstnew(f(list->content))))
-        {
-            ft_lstclear(&list, del);
-            free(list);
-        }
-        ft_lstadd_back(&list, new_list);
-        list = list->next;
-        new_list = new_list->next;
-    }
-    return (new_list);
+	if (!lst || !f || !del)
+		return (NULL);
+	if (!(new_lst = ft_lstnew((f)(lst->content))))
+		return (NULL);
+	save = new_lst;
+	lst = lst->next;
+	while (lst)
+	{
+		if (!(new_lst->next = ft_lstnew((f)(lst->content))))
+		{
+			ft_lstclear(&save, del);
+			return (NULL);
+		}
+		lst = lst->next;
+		new_lst = new_lst->next;
+	}
+	new_lst->next = NULL;
+    return (save);
 }
+	
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,40 +6,34 @@
 /*   By: addzikow <addzikow@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 16:05:57 by addzikow          #+#    #+#             */
-/*   Updated: 2020/12/17 16:08:39 by addzikow         ###   ########lyon.fr   */
+/*   Updated: 2020/12/18 12:01:44 by addzikow         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-size_t ft_strlcat(char *dest, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-    size_t i;
-    size_t dest_length;
-    size_t src_length;
-    // char *tab;
+	size_t	i;
+	size_t	dst_len;
+	size_t	src_len;
 
-    dest_length = ft_strlen(dest);
-    src_length = ft_strlen(src);
-    i = 0;
-    dstsize = 0; //attention JUSTE POUR COMPIL
-    // if (dstsize < dest_length)
-    //     return (src_length + dest_length);
-    // while (src[i] && dest_length + i + 1 < dstsize)
-    // {
-    //     tab[dest_length + i] = src[i];
-    //     i++;
-    // }
-    // dest[dest_length + i] = '\0';
-    return (src_length + dest_length);
+	i = 0;
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (dstsize <= dst_len)
+		return (src_len + dstsize);
+	while ((dst_len + i) < dstsize && src[i])
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	if (dst_len + i == dstsize && dst_len < dstsize)
+		dst[dst_len + --i] = '\0';
+	else
+		dst[dst_len + i] = '\0';
+	if (dst_len > dstsize)
+		return (dstsize + src_len);
+	return (dst_len + src_len);
 }
-
-// int main()
-// {
-//     char *dest = "coucou";
-//     char *src = "beuh";
-//     size_t dstsize = 10;
-//     size_t yo;
-
-//     yo = strlcat(dest, src, dstsize);
-// }
