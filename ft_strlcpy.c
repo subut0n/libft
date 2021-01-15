81038fc75ff721b11500752628118f52a0da5734
subut0n <a.dzikowski@outlook.fr>
2021-01-15 12:31:34 +0100

    bonjour

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,31 +6,24 @@
 /*   By: addzikow <addzikow@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 16:06:05 by addzikow          #+#    #+#             */
-/*   Updated: 2020/12/18 15:11:12 by addzikow         ###   ########lyon.fr   */
+/*   Updated: 2021/01/15 12:31:18 by addzikow         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	size_t i;
-	size_t src_len;
+	size_t len_s;
 
-	i = 0;
-	if (!dst || !src)
+	if (!src)
 		return (0);
-	src_len = ft_strlen(src);
-	if (!dstsize)
-		return (src_len);
-	while (src[i] != '\0' && i < dstsize)
+	len_s = ft_strlen(src);
+	if (size)
 	{
-		dst[i] = src[i];
-		i++;
+		size = len_s + 1 < size ? len_s + 1 : size;
+		ft_bzero(dst, size);
+		ft_memmove(dst, src, size - 1);
 	}
-	if (dstsize < src_len)
-		dst[dstsize - 1] = '\0';
-	else if (dstsize != 0)
-		dst[i] = '\0';
-	return (src_len);
+	return (len_s);
 }
--- libft.h
+++ libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: addzikow <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: addzikow <addzikow@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/18 16:18:54 by addzikow          #+#    #+#             */
-/*   Updated: 2020/12/18 16:39:35 by addzikow         ###   ########lyon.fr   */
+/*   Updated: 2021/01/14 13:58:49 by addzikow         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ int					ft_tolower(int c);
 char				*ft_strchr(const char *s, int c);
 char				*ft_strrchr(const char *s, int c);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
-size_t				ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t				ft_strlcpy(char *dst, const char *src, size_t size);
 size_t				ft_strlcat(char *dst, const char *src, size_t dstsize);
 char				*ft_strnstr(const char *haystack, const char *needle,
 		size_t len);
